9fd23822ea452b2a1f73e1fac4dad292edd0c677
avatarrrrr <marcosrios@pm.me>
2023-05-25 13:38:57 -0300

    [feat] class 2

--- app/src/main/java/br/com/alura/schedule/MainActivity.java
+++ app/src/main/java/br/com/alura/schedule/MainActivity.java
@@ -2,14 +2,27 @@ package br.com.alura.schedule;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hi Avatar!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+
+        List<String> students = Arrays.asList(new String[]{"Avatar", "Aninha", "Vini"});
+
+        ListView studentsView = findViewById(R.id.activity_main_students_list);
+        studentsView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students));
     }
 }
