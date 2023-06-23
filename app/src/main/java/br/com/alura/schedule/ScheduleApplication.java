9444792521e0ab11654975957f6bb4e242980b9d
avatarrrrr <marcosrios@pm.me>
2023-06-23 11:37:08 -0300

    [feat] change icon

--- app/src/main/java/br/com/alura/schedule/ScheduleApplication.java
+++ app/src/main/java/br/com/alura/schedule/ScheduleApplication.java
@@ -14,7 +14,7 @@ public class ScheduleApplication extends Application {
 
     private static void createTestStudents() {
         final StudentsDao studentsDAO = new StudentsDao();
-        studentsDAO.save(new Student("Avatar", "+55 71 99733 3774", "avatar@gmail.com"));
-        studentsDAO.save(new Student("Marcos", "+55 71 99733 3774", "marcosrios@gmail.com"));
+        studentsDAO.save(new Student("Avatar", "+55 11 11111 1111", "avatar@gmail.com"));
+        studentsDAO.save(new Student("Marcos", "+55 22 22222 2222", "marcos@gmail.com"));
     }
 }
